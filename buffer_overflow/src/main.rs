5c7061a0607bdc3320eb067ac9b3fb9e693b50c8
memN0ps <89628341+memN0ps@users.noreply.github.com>
2023-07-28 16:01:28 +1200

    Added empty pool overflow

--- buffer_overflow/src/main.rs
+++ buffer_overflow/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused_imports)]
-use crate::{smep_kpti::exploit_with_kpti, smep_no_kpti::exploit_without_kpti};
+use crate::{
+    pool_overflow_arbitrary_read_write::exploit_pool_overflow_arbitrary_read_write,
+    smep_kpti::exploit_with_kpti, smep_no_kpti::exploit_without_kpti,
+};
 
+mod pool_overflow_arbitrary_read_write;
 mod smep_kpti;
 mod smep_no_kpti;
 
@@ -9,4 +13,5 @@ fn main() {
 
     //exploit_without_kpti();
     exploit_with_kpti();
+    exploit_pool_overflow_arbitrary_read_write();
 }
---
+++ buffer_overflow/src/pool_overflow_arbitrary_read_write.rs
@@ -0,0 +1,3 @@
+#![allow(dead_code)]
+
+pub fn exploit_pool_overflow_arbitrary_read_write() {}
